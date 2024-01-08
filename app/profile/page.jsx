31a139751d6516c1c98fa9fafeb7e55c3e5e35df
GQuadros <gabrielfernand464508@gmail.com>
2024-01-08 13:19:58 -0300

    My Profile start

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,9 +1,45 @@
 'use client';
 
-const Profile = () => {
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Profile from '@components/Profile';
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState([]);
+
+  const handleEdit = () => {
+
+  }
+
+  
+  const handleDelete = async () => {
+    
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    }
+
+    if(session?.user.id) fetchPosts();
+  }, []);
+
   return (
-    <div>Profile</div>
+    <Profile 
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
   )
 }
 
-export default Profile;
+export default MyProfile;
